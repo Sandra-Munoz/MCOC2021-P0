6ab89dbc10578fb593133de645beb29d149021d2
Sandra-Munoz <88350644+Sandra-Munoz@users.noreply.github.com>
2021-09-03 23:28:29 -0400

    Entrega 6

---
+++ Entrega 6/Grafico matriz dispersa solve.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 23:06:10 2021
+
+@author: sandra
+"""
+
+
+import matplotlib.pylab as plt 
+
+
+Ns=[]
+Dts=[] #se guardan lista de listas de tiempo de ensamblado
+Mems=[]#se guardan lista de listas de tiempo de solución
+var=0
+
+Nss=[]
+Dtss=[]
+Memss=[]
+
+for i in range(1):
+    fid=open("CasoMatrizDispersa.txt","r") 
+    for line in fid:
+        var+=1
+        print(line)
+        sl=line.split()
+        N=int(sl[0])
+        dt=float(sl[1])
+        Mem=float(sl[2])
+        
+        
+        Nss.append(N)
+        Dtss.append(dt)
+        
+        Memss.append(Mem)
+        
+  
+        if var==10:
+            Ns.append(Nss)
+            Nss=[]
+            Dts.append(Dtss)
+            Dtss=[]
+            Mems.append(Memss)
+            Memss=[]
+            
+            var=0
+            
+    fid.close()
+    
+import matplotlib.pylab as plt 
+
+
+
+x_v=[10,100,1000,2000,2000,4000,5000,6000,7000,8000]
+
+x1_l = ["10","100","1000","2000","2000","4000","5000","6000","7000","8000"]
+x1_v = [10,100,1000,2000,2000,4000,5000,6000,7000,8000]
+
+y1_l=["0,1 ms","1 ms","10 ms","0.1 s","1 s","10 s","1 min","10 min"]
+y1_v=[0.0001,0.001,0.01,0.1,1,10,60,600]
+
+
+
+
+plt.figure(1)
+
+plt.subplot(2,1,1)
+plt.title("Rendimiento A@B")
+plt.loglog(x_v,Dts[0],marker="o")
+plt.loglog(x_v,Dts[1],marker="o")
+plt.loglog(x_v,Dts[2],marker="o")
+plt.loglog(x_v,Dts[3],marker="o")
+plt.loglog(x_v,Dts[4],marker="o")
+plt.loglog(x_v,Dts[5],marker="o")
+plt.loglog(x_v,Dts[6],marker="o")
+plt.loglog(x_v,Dts[7],marker="o")
+plt.loglog(x_v,Dts[8],marker="o")
+plt.loglog(x_v,Dts[9],marker="o")
+
+
+
+
+plt.xticks(x1_v,x1_l)
+plt.yticks(y1_v,y1_l)
+plt.grid(True)
+plt.xticks(visible=True)
+plt.ylabel("Tiempo de ensamblado")
+
+
+plt.subplot(2,1,2)
+
+
+
+
+
+plt.ylabel("Tiempo de solución")
+plt.xlabel("Tamaño matriz N")
+plt.loglog(x_v,Mems[0],marker="o")
+plt.loglog(x_v,Mems[1],marker="o")
+plt.loglog(x_v,Mems[2],marker="o")
+plt.loglog(x_v,Mems[3],marker="o")
+plt.loglog(x_v,Mems[4],marker="o")
+plt.loglog(x_v,Mems[5],marker="o")
+plt.loglog(x_v,Mems[6],marker="o")
+plt.loglog(x_v,Mems[7],marker="o")
+plt.loglog(x_v,Mems[8],marker="o")
+plt.loglog(x_v,Mems[9],marker="o")
+#plt.loglog(Ns,Mems,marker="o")
+plt.yticks(y1_v,y1_l)
+plt.xticks(x1_v,x1_l,rotation=45)
+#plt.grid(True)
+#plt.xticks(visible=True)
+
+
+
+plt.show()
+
+
+
+
+
+
---
+++ Entrega 6/Matriz llena-inv.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  3 21:43:37 2021
+
+@author: sandra
+"""
+
+
+from scipy import matmul, rand
+from time import perf_counter
+from numpy.linalg import inv, eigh, solve
+from numpy import eye, double, ones
+import scipy.sparse as sparse 
+
+def matriz_laplaciana(N,t=double):
+      e=eye(N)-eye(N,N,1)
+      return t(e+e.T)
+
+
+Ns=[10,100,1000,2000,2000,4000,5000,6000,7000,8000]
+Dts=[]
+Dts2=[]
+fid=open("CasoMatriz llena inv ","w")
+
+for i in range(10):
+    
+    for N in Ns:
+       
+        t1=perf_counter()
+        A=matriz_laplaciana(N)
+        b=matriz_laplaciana(N)
+        t2=perf_counter()
+        x=inv(A)
+        t3=perf_counter()
+        
+        dt=t2-t1 #tiempo ensamblado
+        dt2=t3-t2 #tiempo de solución
+        Dts.append(dt)
+        Dts2.append(dt2)
+        
+        print(f"N={N} dt={dt} s")
+    
+        fid.write(f"{N} {dt} {dt2} \n")
+
+fid.close()
+
+A[0,-1]=1
+A[1,-1]=1
+A[2,-1]=1
+A[3,-1]=1
+
+#print(f"A=\n{A}")
+t4=perf_counter()
+print(t4)
